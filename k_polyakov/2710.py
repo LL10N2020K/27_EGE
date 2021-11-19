107858d0e9663c76b9171fe27e5389565fa65c52
Леонид Дмитриевич Шастин <87826460+LL10N2020K@users.noreply.github.com>
2021-11-19 12:40:36 +0300

    Update 2710.py

--- k_polyakov/2710.py
+++ k_polyakov/2710.py
@@ -1,3 +1,5 @@
+Старое решение:
+
 with open('27-35b.txt') as f:
     n = int(f.readline())
 
@@ -15,4 +17,20 @@ with open('27-35b.txt') as f:
              no[x%2] += 1
              count += yes[x%2]
 
+    print(count)
+
+Новое мое решение:
+
+with open('27-35b.txt') as f:
+    n = int(f.readline())
+    Kall = [0]*2
+    K0 = [0]*2
+    count = 0
+    for i in range(n):
+        x = int(f.readline())
+        if x == 0:
+            K0 = Kall.copy()
+        else:
+            count += K0[(2 - x%2)%2]
+            Kall[x%2] += 1
     print(count)
