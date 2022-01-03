76e60100a80913353a49978bfac73d9bba689ee3
Леонид Дмитриевич Шастин <87826460+LL10N2020K@users.noreply.github.com>
2022-01-03 09:52:35 +0300

    Add files via upload

---
+++ k_polyakov/4848.py
@@ -0,0 +1,19 @@
+un = lambda x: x < 0 and (-x)%10 == 3
+
+with open('27-93b.txt') as f:
+    n, k = map(int, f.readline().split())
+    cUn, s, m = 0, 0, 0
+    INF = float('inf')
+    total = []
+    for j in range(n):
+        x = int(f.readline())
+        cUn += un(x)
+        s += x
+        for prefix in total:
+            if cUn - prefix[1] == k:
+                m = max(m, s - prefix[0])
+            if cUn - prefix[1] > k:
+                total.remove(prefix)
+        total.append([s, cUn])
+    print(m)
+
