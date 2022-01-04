1422de5379d6b0a23ac8335881b509dd04c04c5d
Леонид Дмитриевич Шастин <87826460+LL10N2020K@users.noreply.github.com>
2022-01-04 10:01:01 +0300

    Update 4848.py

--- k_polyakov/4848.py
+++ k_polyakov/4848.py
@@ -17,3 +17,24 @@ with open('27-93b.txt') as f:
         total.append([s, cUn])
     print(m)
 
+#НЕБОЛЬШАЯ ПРАВКА (НЕ БЫЛ ОБРАБОТАН ИСКЛЮЧИТЕЛЬНЫЙ СЛУЧАЙ)
+
+un = lambda x: x < 0 and (-x)%10 == 3
+
+with open('27-93b.txt') as f:
+    n, k = map(int, f.readline().split())
+    cUn, s, m = 0, 0, 0
+    INF = float('inf')
+    total = []
+    for j in range(n):
+        x = int(f.readline())
+        s += x
+        if cUn == k: m = max(m, s)
+        cUn += un(x)
+        for prefix in total:
+            if cUn - prefix[1] == k:
+                m = max(m, s - prefix[0])
+            if cUn - prefix[1] > k:
+                total.remove(prefix)
+        total.append([s, cUn])
+    print(m)
